afd56cbd8d360e9f8628a2f1939cedb0333d7042
Vinicios Barros <vinicios.barros@ieee.org>
2014-07-31 17:11:14 -0300

    indent: change indentation to resembling BSD/Allman style

--- server.c
+++ server.c
@@ -1,10 +1,10 @@
-/*
+/* 
 
-by vinicios - July 2014
+   by vinicios - July 2014
 
-Server vChat - servidor Chat messages
+   Server vChat - servidor Chat messages
 
-*/
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,129 +23,116 @@ Server vChat - servidor Chat messages
 #define BUFSIZE 1024
 
 void
-send_to_all (int j, int i, int sockfd, int nbytes_recvd, char *recv_buf,
-	     fd_set * master)
+send_to_all(int j, int i, int sockfd, int nbytes_recvd, char *recv_buf,
+      fd_set * master)
 {
-  if (FD_ISSET (j, master))
+  if (FD_ISSET(j, master))
+  {
+    if (j != sockfd && j != i)
     {
-      if (j != sockfd && j != i)
-	{
-	  if (send (j, recv_buf, nbytes_recvd, 0) == -1)
-	    {
-	      perror ("send");
-	    }
-	}
+      if (send(j, recv_buf, nbytes_recvd, 0) == -1)
+      {
+        perror("send");
+      }
     }
+  }
 }
 
-void
-send_recv (int i, fd_set * master, int sockfd, int fdmax)
+void send_recv(int i, fd_set * master, int sockfd, int fdmax)
 {
   int nbytes_recvd, j;
   char recv_buf[BUFSIZE], buf[BUFSIZE];
 
-  nbytes_recvd = recv (i, recv_buf, BUFSIZE, 0);
+  nbytes_recvd = recv(i, recv_buf, BUFSIZE, 0);
   switch (nbytes_recvd)
+  {
+  case 0:
+    syslog(LOG_NOTICE,
+         "socket %d was unexpected terminated by remote\n", i);
+    close(i);
+    FD_CLR(i, master);
+  case -1:
+    syslog(LOG_NOTICE, "Error: socket %d will be terminated\n", i);
+    close(i);
+    FD_CLR(i, master);
+  default:
+    for (j = 0; j <= fdmax; j++)
     {
-    case 0:
-      syslog (LOG_NOTICE,
-       "socket %d was unexpected terminated by remote\n", i);
-      close (i);
-      FD_CLR (i, master);
-    case -1:
-      syslog (LOG_NOTICE, "Error: socket %d will be terminated\n", i);
-      close (i);
-      FD_CLR (i, master);
-    default:
-      for (j = 0; j <= fdmax; j++)
-      {
-	     send_to_all (j, i, sockfd, nbytes_recvd, recv_buf, master);
-      }
+      send_to_all(j, i, sockfd, nbytes_recvd, recv_buf, master);
     }
+  }
 
-  /*
-     if ((nbytes_recvd = recv(i, recv_buf, BUFSIZE, 0)) <= 0) {
-     if (nbytes_recvd == 0) {
-     syslog(LOG_NOTICE,"socket %d terminated\n", i);
-     }else {
-     perror("recv");
-     }
-     close(i);
-     FD_CLR(i, master);
-     }else {
-     //printf("%s\n", recv_buf);
-     for(j = 0; j <= fdmax; j++){
-     send_to_all(j, i, sockfd, nbytes_recvd, recv_buf, master );
-     }
-     }
-   */
+  /* 
+     if ((nbytes_recvd = recv(i, recv_buf, BUFSIZE, 0)) <= 0) { if
+     (nbytes_recvd == 0) { syslog(LOG_NOTICE,"socket %d terminated\n", i);
+     }else { perror("recv"); } close(i); FD_CLR(i, master); }else {
+     //printf("%s\n", recv_buf); for(j = 0; j <= fdmax; j++){ send_to_all(j, 
+     i, sockfd, nbytes_recvd, recv_buf, master ); } } */
 }
 
 void
-connection_accept (fd_set * master, int *fdmax, int sockfd,
-		   struct sockaddr_in *client_addr)
+connection_accept(fd_set * master, int *fdmax, int sockfd,
+          struct sockaddr_in *client_addr)
 {
   socklen_t addrlen;
   int newsockfd;
 
-  addrlen = sizeof (struct sockaddr_in);
+  addrlen = sizeof(struct sockaddr_in);
   if ((newsockfd =
-       accept (sockfd, (struct sockaddr *) client_addr, &addrlen)) == -1)
-    {
-      perror ("accept");
-      exit (1);
-    }
+     accept(sockfd, (struct sockaddr *)client_addr, &addrlen)) == -1)
+  {
+    perror("accept");
+    exit(1);
+  }
   else
-    {
-      FD_SET (newsockfd, master);
-      if (newsockfd > *fdmax)
-	     *fdmax = newsockfd;
+  {
+    FD_SET(newsockfd, master);
+    if (newsockfd > *fdmax)
+      *fdmax = newsockfd;
 
-      syslog (LOG_NOTICE, "Connection established with %s on port %d \n",
-	      inet_ntoa (client_addr->sin_addr),
-	      ntohs (client_addr->sin_port));
-    }
+    syslog(LOG_NOTICE, "Connection established with %s on port %d \n",
+         inet_ntoa(client_addr->sin_addr), ntohs(client_addr->sin_port));
+  }
 }
 
-void
-connect_request (int *sockfd, struct sockaddr_in *my_addr)
+void connect_request(int *sockfd, struct sockaddr_in *my_addr)
 {
   int yes = 1;
 
-  if ((*sockfd = socket (AF_INET, SOCK_STREAM, 0)) == -1)
-    {
-      perror ("Socket");
-      exit (1);
-    }
+  if ((*sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+  {
+    perror("Socket");
+    exit(1);
+  }
 
   my_addr->sin_family = AF_INET;
-  my_addr->sin_port = htons (PORT);
+  my_addr->sin_port = htons(PORT);
   my_addr->sin_addr.s_addr = INADDR_ANY;
-  memset (my_addr->sin_zero, '\0', sizeof my_addr->sin_zero);
+  memset(my_addr->sin_zero, '\0', sizeof my_addr->sin_zero);
 
-  if (setsockopt (*sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof (int)) == -1)
+  if (setsockopt(*sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
   {
-      perror ("setsockopt");
-      exit (1);
+    perror("setsockopt");
+    exit(1);
   }
 
-  if (bind (*sockfd, (struct sockaddr *) my_addr, sizeof (struct sockaddr)) == -1)
+  if (bind(*sockfd, (struct sockaddr *)my_addr, 
+    sizeof(struct sockaddr)) ==-1)
   {
-      perror ("Unable to bind");
-      exit (1);
+    perror("Unable to bind");
+    exit(1);
   }
-  if (listen (*sockfd, 10) == -1)
-    {
-      perror ("listen");
-      exit (1);
-    }
-  //printf("\n TCPServer Waiting for client on port %d\n", PORT);
-  syslog (LOG_NOTICE, "Listening clients on port %d", PORT);
-  fflush (stdout);
+  if (listen(*sockfd, 10) == -1)
+  {
+    perror("listen");
+    exit(1);
+  }
+  // printf("\n TCPServer Waiting for client on port %d\n", PORT);
+  syslog(LOG_NOTICE, "Listening clients on port %d", PORT);
+  fflush(stdout);
 }
 
-int
-main ()
+int main()
 {
   fd_set master;
   fd_set read_fds;
@@ -154,39 +141,39 @@ main ()
   struct sockaddr_in my_addr, client_addr;
 
   // deamonize the server
-  daemon (0, 0);
-  openlog ("vChatServer", LOG_PID, LOG_DAEMON);
-  syslog (LOG_NOTICE, "vCHAT in the flow");
+  daemon(0, 0);
+  openlog("vChatServer", LOG_PID, LOG_DAEMON);
+  syslog(LOG_NOTICE, "vCHAT in the flow");
 
 
-  FD_ZERO (&master);
-  FD_ZERO (&read_fds);
-  connect_request (&sockfd, &my_addr);
-  FD_SET (sockfd, &master);
+  FD_ZERO(&master);
+  FD_ZERO(&read_fds);
+  connect_request(&sockfd, &my_addr);
+  FD_SET(sockfd, &master);
 
   fdmax = sockfd;
 
-  syslog (LOG_NOTICE, "vCHAT will be looped");
+  syslog(LOG_NOTICE, "vCHAT will be looped");
 
   while (1)
+  {
+    read_fds = master;
+    if (select(fdmax + 1, &read_fds, NULL, NULL, NULL) == -1)
     {
-      read_fds = master;
-      if (select (fdmax + 1, &read_fds, NULL, NULL, NULL) == -1)
-	{
-	  perror ("select");
-	  exit (4);
-	}
-
-      for (i = 0; i <= fdmax; i++)
-	{
-	  if (FD_ISSET (i, &read_fds))
-	    {
-	      if (i == sockfd)
-		connection_accept (&master, &fdmax, sockfd, &client_addr);
-	      else
-		send_recv (i, &master, sockfd, fdmax);
-	    }
-	}
+      perror("select");
+      exit(4);
     }
+
+    for (i = 0; i <= fdmax; i++)
+    {
+      if (FD_ISSET(i, &read_fds))
+      {
+        if (i == sockfd)
+          connection_accept(&master, &fdmax, sockfd, &client_addr);
+        else
+          send_recv(i, &master, sockfd, fdmax);
+      }
+    }
+  }
   return 0;
 }
