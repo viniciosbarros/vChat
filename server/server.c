1a47c7a4a5b68335150f927390c108d5d89a8791
Vinicios Barros <vinicios.barros@ieee.org>
2014-08-26 12:20:55 -0300

    not-clean: include a nonblock routine to server listener socket. obs: not clean commit because of comments of some unimplemented functions

--- server/server.c
+++ server/server.c
@@ -17,6 +17,7 @@ Server vChat - servidor Chat messages
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <syslog.h>
 #include <signal.h>
 
@@ -28,30 +29,70 @@ Server vChat - servidor Chat messages
 #define BUFSIZE 1024
 
 static struct event ev_accept;
+static struct event_base *ev_base;
 
 TAILQ_HEAD(, client) client_list;
-TAILQ_INIT(&client_list);
 
 #define DPRINTF(fmt, args...) \
 	fprintf(stderr, "D: " fmt "\n", ## args);
 
 void send_to_all(struct bufferevent *, void *);
+/*void send_to_all(int, int, int, int, char *, fd_set*);*/
 void send_recv(int, fd_set *, int, int);
 static void connection_accept(int, short, void *);
 static void connect_request(int *);
+int changeTononblock(int *);
+
+
+int 
+changeTononblock(int *fd)
+{
+	int flags;
+
+	flags = fcntl(*fd, F_GETFL);
+	if (flags < 0)
+		return flags;
+	flags |= O_NONBLOCK;
+	if (fcntl(*fd, F_SETFL, flags) < 0)
+		return -1;
+
+	return 0;
+}
+
 
 void
-send_to_all(int j, int i, int sockfd, int nbytes_recvd, char *recv_buf,
-    fd_set * master)
-/*send_to_all(struct bufferevent *buf_ev, void *arg)*/
+/*send_to_all(int j, int i, int sockfd, int nbytes_recvd, char *recv_buf,
+    fd_set * master)*/
+send_to_all(struct bufferevent *buf_ev, void *arg)
 {
-	if (FD_ISSET(j, master)) {
+	/*if (FD_ISSET(j, master)) {
 		if (j != sockfd && j != i) {
 			if (send(j, recv_buf, nbytes_recvd, 0) == -1) {
 				perror("send");
 			}
 		}
+	}*/
+
+	size_t n;
+	struct client *c = arg;
+	struct client *client;
+	uint8_t data[BUFSIZE];
+
+	while(1){
+		n = bufferevent_read(buf_ev, data, sizeof(data));
+		if (n <= 0){
+			bufferevent_free(buf_ev);
+			close(c->fd);
+			free(c);
+		}
+
+		TAILQ_FOREACH(client, &client_list, entries) {
+			if (client != c) {
+				bufferevent_write(client->buf_ev, data, n);
+			}
+		}
 	}
+
 }
 
 void
@@ -76,6 +117,35 @@ send_recv(int i, fd_set * master, int sockfd, int fdmax)
 			    master, void *void);*/
 		}
 	}
+}
+
+void
+peer_read_cb(struct bufferevent *bufev, void *bula)
+{
+	struct client *c = bula;
+	struct client *client;
+	size_t dlen;
+	char buf[512];
+
+	dlen = bufferevent_read(bufev, buf, sizeof(buf));
+	if (dlen == 0) {
+		bufferevent_free(bufev);
+		close(c->fd);
+		free(c);
+
+		fprintf(stderr, "Client disconnected\n");
+	}
+
+	buf[dlen] = 0;
+
+	fprintf(stderr, "### %s\n", buf);
+
+	TAILQ_FOREACH(client, &client_list, entries) {
+		/* if (client != c) { */ /*Send to all even the sender*/
+			bufferevent_write(client->buf_ev, buf, dlen);
+		/*}*/
+	}
+
 }
 
 static void
@@ -103,7 +173,10 @@ connection_accept(int fd, short event, void *bula)
 	c->fd = newsockfd;
 	TAILQ_INSERT_HEAD(&client_list, c, entries);
 
-	c->buf_ev = bufferevent_new(c->fd, peer_read_cb, NULL, NULL, c);
+	c->buf_ev = bufferevent_socket_new(ev_base, newsockfd, 0);
+	bufferevent_setcb(c->buf_ev, peer_read_cb, NULL, NULL, c);
+
+	/*c->buf_ev = bufferevent_new(c->fd, peer_read_cb, NULL, NULL, c);*/
 	bufferevent_enable(c->buf_ev, EV_READ);
 
 	DPRINTF("Connection established with %s on port %d \n",
@@ -122,8 +195,13 @@ connect_request(int *sockfd)
 
 	memset(&sin, 0, sizeof(sin));
 	sin.sin_family = AF_INET;
-	sin.sin_port = htons(PORT);
 	sin.sin_addr.s_addr = INADDR_ANY;
+	sin.sin_port = htons(PORT);
+
+	if (bind(*sockfd, (struct sockaddr *) &sin, sizeof(sin)) == -1) {
+		close(*sockfd);
+		err(1, "bind");
+	}
 
 	if (setsockopt(*sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
 	    sizeof(int)) == -1) {
@@ -131,14 +209,14 @@ connect_request(int *sockfd)
 		err(1, "setsockopt");
 	}
 
-	if (bind(*sockfd, (struct sockaddr *) &sin, sizeof(sin)) == -1) {
-		close(*sockfd);
-		err(1, "bind");
-	}
-
 	if (listen(*sockfd, 10) == -1) {
 		close(*sockfd);
 		err(1, "listen");
+	}
+
+	if(changeTononblock(sockfd) < 0){
+		close(*sockfd);
+		err(1, "change to non-block");	
 	}
 
 	DPRINTF("Listening clients on port %d", PORT);
@@ -155,6 +233,8 @@ main(int argc, char *argv[])
 	DPRINTF("vCHAT in the flow");
 
 	event_init();
+
+	TAILQ_INIT(&client_list);
 
 	connect_request(&sockfd);
 
